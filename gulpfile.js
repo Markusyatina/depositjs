0874ad6950ec6cdc47d571c8026420b83ed96e6c
ReklatsMasters <dmitrycvet@gmail.com>
2015-01-28 01:06:04 +0500

    [=] gulp: remove `watch` task, added `clean` task

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,28 @@
 'use strict';
 
-var browserify = require('browserify'),
-		watchify = require('watchify'),
-		gulp = require('gulp'),
-		source = require('vinyl-source-stream'),
-		sourceFile = './userjs.js',
-		destFolder = './dist/',
-		destFile = 'dist.js';
+var browserify = require('browserify');
+var gulp       = require('gulp');
+var source     = require('vinyl-source-stream');
+var sourceFile = './userjs.js';
+var destFolder = './dist/';
+var destFile   = 'dist.min.js';
+var uglify     = require('gulp-uglify');
+var bufferify  = require('vinyl-buffer');
+var del        = require('del');
+var vinylPaths = require('vinyl-paths');
  
 gulp.task('browserify', function() {
 	return browserify(sourceFile)
 	.bundle()
 	.pipe(source(destFile))
+	.pipe(bufferify())
+	.pipe(uglify())
 	.pipe(gulp.dest(destFolder));
 });
 
-gulp.task('watch', function() {
-	var bundler = watchify(sourceFile);
- 
-	function rebundle() {
-		return bundler.bundle()
-			.pipe(source(destFile))
-			.pipe(gulp.dest(destFolder));
-	}
-
-	bundler.on('update', rebundle);
-	return rebundle();
+gulp.task('clean:tmp', function () {
+	return gulp.src(destFolder)
+		.pipe(vinylPaths(del));
 });
 
-gulp.task('default', ['browserify', 'watch']);
+gulp.task('default', ['clean:tmp', 'browserify']);
